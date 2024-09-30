c2a57ea0015f108a31a4a4590491936314581d8d
guzelmus <guzelmusa307@gmail.com>
2024-09-30 21:43:09 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -3,10 +3,10 @@ export default function Privacy() {
     return (
         <div>
             <h1 className="font-semibold text-3xl text-white/75">Gizlilik Politikası</h1>
-            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 28/07/2024 🆕️</p>
+            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 01/10/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ Botun Sorunsuz Çalışabilmesi İçin Yönetici Yektisi Verip Entegraston Rolünü En Üste Taşımanız Gerekir. <a className="text-white">(Slayer Bot Geliştirici Ekibi olarak, kişisel verilerinizin gizliliğini ve güvenliğini korumak bizim için çok önemlidir. Bu nedenle, Discord sunucularımızı ziyaret ettiğinizde veya Slayer Bot'u kullandığınızda elde ettiğimiz kişisel verilerinizi nasıl topladığımızı, kullandığımızı ve paylaştığımızı açıklayan bu Gizlilik Politikasını hazırladık.
-Bu Gizlilik Politikası, geliştirici ekibimiz tarafından sunulan Slayer Discord botunu kullanan tüm kullanıcılar için geçerlidir.)</a></p>
+        <p>⊳ Botun Sorunsuz Çalışabilmesi İçin Yönetici Yektisi Verip Entegraston Rolünü En Üste Taşımanız Gerekir. <a className="text-white">(Svex Bot Geliştirici Ekibi olarak, kişisel verilerinizin gizliliğini ve güvenliğini korumak bizim için çok önemlidir. Bu nedenle, Discord sunucularımızı ziyaret ettiğinizde veya Svex'i kullandığınızda elde ettiğimiz kişisel verilerinizi nasıl topladığımızı, kullandığımızı ve paylaştığımızı açıklayan bu Gizlilik Politikasını hazırladık.
+Bu Gizlilik Politikası, geliştirici ekibimiz tarafından sunulan Svex Discord botunu kullanan tüm kullanıcılar için geçerlidir.)</a></p>
         <div className="py-3"></div>
         <p>⊳ Bot ile etkileşim kurarken saklanan verileriniz arasında mesaj içeriğiniz, durumunuz, kullanıcı adınız, ID numaranız, sunucucu ID numaranız, kanal adı ve ID numarası, hesap oluşturma tarihiniz, sunucu üye sayısı, bot eklenme tarihi ve sonradan eklenenilecek veriler yer alır.</p>
         <div className="py-3"></div>
