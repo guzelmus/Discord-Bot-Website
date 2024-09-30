c4ff3bc6b310da8fc67aa1b6c0c6f6aa8cb193e5
guzelmus <guzelmusa307@gmail.com>
2024-09-30 21:40:21 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,9 +2,9 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Kullanım Şartları</h2>
-      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 28/07/2024 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Son Güncelleme: 01/10/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Slayer Bot tamamen ücretsiz sunulmaktadır, botu ücret karşılığı satmaya çalışmak kesinlikle yasaktır.</p>
+<p>⊳ Svex tamamen ücretsiz sunulmaktadır, botu ücret karşılığı satmaya çalışmak kesinlikle yasaktır.</p>
 <div className="py-3"></div>
 <p>⊳ Botun herhangi bir hatasından faydalanmak yasaktır.</p>
 <div className="py-3"></div>
@@ -12,9 +12,9 @@ export default function Tos() {
 <div className="py-3"></div>
 <p>⊳ Botu kopyalamaya çalışmak kesinlikle yasaktır ve cezai sorumluluk taşır.</p>
 <div className="py-3"></div>
-<p>⊳ Yukarıdaki maddeler için veritbanında log tutulmaktadır, tespi edilirse gerekli işlemler uygulanacaktır. Slayer Bot olduğu gibi sunulmaktadır, geliştiriciler botun işleyişinden veya kullanımından kaynaklanan herhangi bir sorumluluk kabul etmez.</p>
+<p>⊳ Yukarıdaki maddeler için veritbanında log tutulmaktadır, tespi edilirse gerekli işlemler uygulanacaktır. Svex olduğu gibi sunulmaktadır, geliştiriciler botun işleyişinden veya kullanımından kaynaklanan herhangi bir sorumluluk kabul etmez.</p>
 <div className="py-3"></div>
-<p>⊳ Belirli Komutları Kullanmak İçin <a className="text-white" href="https://top.gg/bot/1201613667561639947">Slayer Bot'a</a> Oy Vermeniz Gerekir.</p>
+<p>⊳ Belirli Komutları Kullanmak İçin <a className="text-white" href="https://top.gg/bot/1201613667561639947">Svex'e</a> Oy Vermeniz Gerekir.</p>
 </div>
     </div>
   );
